c2efbf772663df5df8743b1f5fd47e483c3d9512
1298743328szyasy <1298743328@qq.com>
2019-06-20 11:47:22 +0800

    店铺信息的封装

--- src/main/java/cn/imnu/o2o/entity/Shop.java
+++ src/main/java/cn/imnu/o2o/entity/Shop.java
@@ -29,5 +29,89 @@ public class Shop {
 	private PersonInfo owner;
 	//店铺类别
 	private ShopCategory shopCategory;
+	public Long getShopId() {
+		return shopId;
+	}
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopDesc() {
+		return shopDesc;
+	}
+	public void setShopDesc(String shopDesc) {
+		this.shopDesc = shopDesc;
+	}
+	public String getShopAddr() {
+		return shopAddr;
+	}
+	public void setShopAddr(String shopAddr) {
+		this.shopAddr = shopAddr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getShopImg() {
+		return shopImg;
+	}
+	public void setShopImg(String shopImg) {
+		this.shopImg = shopImg;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+	public String getAdvice() {
+		return advice;
+	}
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+	public Area getArea() {
+		return area;
+	}
+	public void setArea(Area area) {
+		this.area = area;
+	}
+	public PersonInfo getOwner() {
+		return owner;
+	}
+	public void setOwner(PersonInfo owner) {
+		this.owner = owner;
+	}
+	public ShopCategory getShopCategory() {
+		return shopCategory;
+	}
+	public void setShopCategory(ShopCategory shopCategory) {
+		this.shopCategory = shopCategory;
+	}
 	
 }

9960622c61964bb0810fe13e3ce82a41a926c854
1298743328szyasy <1298743328@qq.com>
2019-06-21 23:55:30 +0800

    类型转化工具类

---
+++ src/main/java/cn/imnu/o2o/util/HttpServletRequestUtil.java
@@ -0,0 +1,49 @@
+package cn.imnu.o2o.util;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+
+public class HttpServletRequestUtil {
+	public static int getInt(HttpServletRequest request,String key) {
+		try {
+			//将字符串转化为整型
+			return Integer.decode(request.getParameter(key));
+		}catch(Exception e) {
+			return -1;
+		}
+	}
+	public static long getLong(HttpServletRequest request,String key) {
+		try {
+			//将字符串转化为长整型
+			return Long.valueOf(request.getParameter(key));
+		}catch(Exception e) {
+			return -1;
+		}
+	}
+	public static Double getDouble(HttpServletRequest request,String key) {
+		try {
+			return Double.valueOf(request.getParameter(key));
+		}catch(Exception e) {
+			return -1d;
+		}
+	}
+	public static boolean getBoolean(HttpServletRequest request,String key) {
+		try {
+			return Boolean.valueOf(request.getParameter(key));
+		}catch(Exception e) {
+			return false;
+		}
+	}
+	public static String getString(HttpServletRequest request,String key) {
+		try {
+			String result = request.getParameter(key);
+			if(result!=null) {
+				result=result.trim();
+			}if("".equals(result)) {
+				result = null;
+			}
+			return result;
+		}catch(Exception e) {
+			return null;
+		}
+	}
+}

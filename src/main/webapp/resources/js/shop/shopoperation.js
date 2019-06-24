32c329ec94270557b783577db104ecb050f5f58d
1298743328szyasy <1298743328@qq.com>
2019-06-24 23:59:04 +0800

    前端js

--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -16,7 +16,7 @@ $(function(){
 				data.areaList.map(function(item,index){
 					tempAreaHtml+='<option data-id="'+item.areaId+'">'+item.areaName+'</option>';
 				});
-				$('#shopCategory').html(tempHtml);
+				$('#shop-category').html(tempHtml);
 				$('#area').html(tempAreaHtml);
 			}
 		});
@@ -24,33 +24,34 @@ $(function(){
 			var shop = {};
 			shop.shopName=$('#shop-name').val();
 			shop.shopAddr=$('#shop-addr').val();
-			shop.phone=$('shop-phone').val();
-			shop.shopDesc=$('shop-desc').val();
-			shop.shopCategory={
-					shopCategoryId:$('#shop-category').find('option').not(function(){
+			shop.phone=$('#shop-phone').val();
+			shop.shopDesc=$('#shop-desc').val();
+			shop.shopCategory = {
+					shopCategoryId : $('#shop-category').find('option').not(function() {
 						return !this.selected;
 					}).data('id')
-			};
-			shop.area={
-					areaId:$('#area').find('option').not(function(){
+				};
+			shop.area = {
+					areaId : $('#area').find('option').not(function() {
 						return !this.selected;
 					}).data('id')
-			};
+				};
 			var shopImg=$('#shop-img')[0].files[0];
 			var formData = new FormData();
 			formData.append('shopImg',shopImg);
 			formData.append('shopStr',JSON.stringify(shop));
-			var verifyCodeCodeActual = $('#j_captcha').val();
+			var verifyCodeActual = $('#j_captcha').val();
 			if(!verifyCodeActual){
 				$.toast('请输入验证码！');
 				return;
 			}
+			formData.append('verifyCodeActual', verifyCodeActual);
 			$.ajax({
 				url:registerShopUrl,
 				type:'POST',
 				data:formData,
 				contentType:false,
-				proceesData:false,
+				processData:false,
 				cache:false,
 				success:function(data){
 					if(data.success){

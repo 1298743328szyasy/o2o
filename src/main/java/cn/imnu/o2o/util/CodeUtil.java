b3b55c4395bc2ef440748c6c615e68d35866536c
1298743328szyasy <1298743328@qq.com>
2019-06-24 23:58:36 +0800

    验证码

--- src/main/java/cn/imnu/o2o/util/CodeUtil.java
+++ src/main/java/cn/imnu/o2o/util/CodeUtil.java
@@ -3,12 +3,17 @@ package cn.imnu.o2o.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class CodeUtil {
+	/**
+	 * 检查验证码是否和预期相符
+	 * 
+	 * @param request
+	 * @return
+	 */
 	public static boolean checkVerifyCode(HttpServletRequest request) {
-		//前台显示验证码
-		String verifyCodeExpected=(String)request.getSession().getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_CONFIG_KEY);
-		//输入验证码
-		String verifyCodeActual = HttpServletRequestUtil.getString(request,"verifyCodeActual");
-		if(verifyCodeActual==null||!verifyCodeActual.equals(verifyCodeExpected)) {
+		String verifyCodeExpected = (String) request.getSession()
+				.getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
+		String verifyCodeActual = HttpServletRequestUtil.getString(request, "verifyCodeActual");
+		if (verifyCodeActual == null || !verifyCodeActual.equals(verifyCodeExpected)) {
 			return false;
 		}
 		return true;

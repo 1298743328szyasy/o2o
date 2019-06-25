a0bd068bec4cbb2fe835836fd320c08374913b45
1298743328szyasy <1298743328@qq.com>
2019-06-25 23:37:08 +0800

    店铺列表和总数

--- src/test/java/cn/imnu/o2o/dao/ShopDaoTest.java
+++ src/test/java/cn/imnu/o2o/dao/ShopDaoTest.java
@@ -3,6 +3,7 @@ package cn.imnu.o2o.dao;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -17,6 +18,22 @@ import cn.imnu.o2o.entity.ShopCategory;
 public class ShopDaoTest extends BaseTest{
 	@Autowired
 	private ShopDao shopDao;
+	
+	@Test
+	public void testQueryShopListAndCount() {
+		Shop shopCondition = new Shop();
+		PersonInfo owner= new PersonInfo();
+		owner.setUserId(1L);
+		shopCondition.setOwner(owner);
+		List<Shop> shopList = shopDao.queryShopList(shopCondition,0,5);
+		int count = shopDao.queryShopCount(shopCondition);
+		System.out.println("店铺列表的大小:"+shopList.size());
+		System.out.println("店铺总数:"+count);
+		ShopCategory sc = new ShopCategory();
+		sc.setShopCategoryId(3L);
+		shopCondition.setShopCategory(sc);
+		
+	}
 	@Test
 	@Ignore
 	public void testQueryByShopId() {
@@ -26,7 +43,7 @@ public class ShopDaoTest extends BaseTest{
 		System.out.println("areaName:"+shop.getArea().getAreaName());
 	}
 	@Test
-	
+	@Ignore
 	public void testInsertShop() {
 		Shop shop = new Shop();
 		PersonInfo owner = new PersonInfo();

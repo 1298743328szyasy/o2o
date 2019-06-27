236a35ff654bfcd4fa7ccc3adf99fddce3afa786
1298743328szyasy <1298743328@qq.com>
2019-06-27 15:37:31 +0800

    错误类

---
+++ src/main/java/cn/imnu/o2o/exceptions/ProductCategoryOperationException.java
@@ -0,0 +1,13 @@
+package cn.imnu.o2o.exceptions;
+
+public class ProductCategoryOperationException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1681603280007512292L;
+	
+	public ProductCategoryOperationException(String msg) {
+		super(msg);
+	}
+}

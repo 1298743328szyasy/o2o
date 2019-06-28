909038e9dd6a5c129df379147f0dd826060987b2
1298743328szyasy <1298743328@qq.com>
2019-06-28 23:29:25 +0800

    图片dao实现

---
+++ src/main/java/cn/imnu/o2o/dao/ProductImgDao.java
@@ -0,0 +1,17 @@
+package cn.imnu.o2o.dao;
+
+import java.util.List;
+
+import cn.imnu.o2o.entity.ProductImg;
+
+public interface ProductImgDao {
+	List<ProductImg> queryProductImgList(long productId);
+	/**
+	 * 批量添加商品详情图片
+	 * @param productImgList
+	 * @return
+	 */
+	int batchInsertProductImg(List<ProductImg> productImgList);
+	
+	int deleteProductImgByProductId(long productId);
+}

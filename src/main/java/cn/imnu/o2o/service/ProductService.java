840731a39c86a66dd14bc4ad7ce7957e46806db1
1298743328szyasy <1298743328@qq.com>
2019-06-30 21:30:33 +0800

    添加修改删除商品信息的业务

--- src/main/java/cn/imnu/o2o/service/ProductService.java
+++ src/main/java/cn/imnu/o2o/service/ProductService.java
@@ -1,22 +1,54 @@
 package cn.imnu.o2o.service;
 
-import java.util.List;
-
 import cn.imnu.o2o.dto.ImageHolder;
 import cn.imnu.o2o.dto.ProductExecution;
 import cn.imnu.o2o.entity.Product;
 import cn.imnu.o2o.exceptions.ProductOperationException;
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.List;
 
 public interface ProductService {
+
+	/**
+	 * 查询商品列表并分页，可输入的条件有： 商品名（模糊），商品状态，店铺Id,商品类别
+	 *
+	 * @param productCondition
+	 * @param pageIndex
+	 * @param pageSize
+	 * @return
+	 */
+	ProductExecution getProductList(Product productCondition, int pageIndex, int pageSize);
+
+	/**
+	 * 通过商品Id查询唯一的商品信息
+	 *
+	 * @param productId
+	 * @return
+	 */
+	Product getProductById(long productId);
+
 	/**
 	 * 添加商品信息以及图片处理
 	 * 
 	 * @param product
-	 * @param thumbnail缩略图
-	 * @param productImgList详情图
+	 * @param thumbnail
+	 * @param productImgList
 	 * @return
 	 * @throws ProductOperationException
 	 */
 	ProductExecution addProduct(Product product, ImageHolder thumbnail, List<ImageHolder> productImgList)
+			throws ProductOperationException;
+
+	/**
+	 * 修改商品信息以及图片处理
+	 *
+	 * @param product
+	 * @param thumbnail
+	 * @param productImgHolderList
+	 * @return
+	 * @throws ProductOperationException
+	 */
+	ProductExecution modifyProduct(Product product, ImageHolder thumbnail, List<ImageHolder> productImgHolderList)
 			throws ProductOperationException;
 }

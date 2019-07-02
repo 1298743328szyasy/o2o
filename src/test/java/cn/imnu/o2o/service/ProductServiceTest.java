1d74be7af7cd790920b1f38986941036fa1ba4c9
1298743328szyasy <1298743328@qq.com>
2019-07-02 23:46:38 +0800

    测试修改商品信息

--- src/test/java/cn/imnu/o2o/service/ProductServiceTest.java
+++ src/test/java/cn/imnu/o2o/service/ProductServiceTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,11 +23,14 @@ import cn.imnu.o2o.entity.ProductCategory;
 import cn.imnu.o2o.entity.Shop;
 import cn.imnu.o2o.enums.ProductStateEnum;
 import cn.imnu.o2o.exceptions.ProductOperationException;
+import cn.imnu.o2o.exceptions.ShopOperationException;
 
 public class ProductServiceTest extends BaseTest{
 	@Autowired
 	private ProductService productService;
+	
 	@Test
+	@Ignore
 	public void testAddProduct() throws ProductOperationException, FileNotFoundException, UnsupportedEncodingException{
 		Product product = new Product();
 		Shop shop=new Shop();
@@ -52,6 +56,31 @@ public class ProductServiceTest extends BaseTest{
 		productImgList.add(new ImageHolder(productImg1.getName(),is1));
 		productImgList.add(new ImageHolder(productImg2.getName(),is2));
 		ProductExecution pe = productService.addProduct(product, thumbnail, productImgList);
+		assertEquals(ProductStateEnum.SUCCESS.getState(),pe.getState());
+	}
+	@Test
+	public void testModifyProduct() throws ShopOperationException,FileNotFoundException{
+		Product product=new Product();
+		Shop shop = new Shop();
+		shop.setShopId(1L);
+		ProductCategory pc=new ProductCategory();
+		pc.setProductCategoryId(1L);
+		product.setProductId(1L);
+		product.setShop(shop);
+		product.setProductCategory(pc);
+		product.setProductName("正式商品");
+		product.setProductDesc("正式商品");
+		File thumbnailFile = new File("/Users/baidu/work/image/timg.jpg");
+		InputStream is = new FileInputStream(thumbnailFile);
+		ImageHolder thumbnail = new ImageHolder(thumbnailFile.getName(),is);
+		File productImg1 = new File("/Users/baidu/work/image/huangjiaju.jpg");
+		InputStream is1 = new FileInputStream(productImg1);
+		File productImg2 = new File("/Users/baidu/work/image/dabai.jpg");
+		InputStream is2 = new FileInputStream(productImg2);
+		List<ImageHolder> productImgList = new ArrayList<ImageHolder>();
+		productImgList.add(new ImageHolder(productImg1.getName(),is1));
+		productImgList.add(new ImageHolder(productImg2.getName(),is2));
+		ProductExecution pe=productService.modifyProduct(product, thumbnail, productImgList);
 		assertEquals(ProductStateEnum.SUCCESS.getState(),pe.getState());
 	}
 }

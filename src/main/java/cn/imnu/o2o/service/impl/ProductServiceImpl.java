7e0adb006f0cf83c4d4ccd873666f63cda85590a
1298743328szyasy <1298743328@qq.com>
2019-07-04 23:58:14 +0800

    查询商品

--- src/main/java/cn/imnu/o2o/service/impl/ProductServiceImpl.java
+++ src/main/java/cn/imnu/o2o/service/impl/ProductServiceImpl.java
@@ -1,5 +1,13 @@
 package cn.imnu.o2o.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import cn.imnu.o2o.dao.ProductDao;
 import cn.imnu.o2o.dao.ProductImgDao;
 import cn.imnu.o2o.dto.ImageHolder;
@@ -13,14 +21,6 @@ import cn.imnu.o2o.util.ImageUtil;
 import cn.imnu.o2o.util.PageCalculator;
 import cn.imnu.o2o.util.PathUtil;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 @Service
 public class ProductServiceImpl implements ProductService {
 	@Autowired
@@ -28,18 +28,6 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	private ProductImgDao productImgDao;
 
-	@Override
-	public ProductExecution getProductList(Product productCondition, int pageIndex, int pageSize) {
-		// 页码转换成数据库的行码，并调用dao层取回指定页码的商品列表
-		int rowIndex = PageCalculator.calculateRowIndex(pageIndex, pageSize);
-		List<Product> productList = productDao.queryProductList(productCondition, rowIndex, pageSize);
-		// 基于同样的查询条件返回该查询条件下的商品总数
-		int count = productDao.queryProductCount(productCondition);
-		ProductExecution pe = new ProductExecution();
-		pe.setProductList(productList);
-		pe.setCount(count);
-		return pe;
-	}
 
 	@Override
 	public Product getProductById(long productId) {
@@ -85,48 +73,7 @@ public class ProductServiceImpl implements ProductService {
 		}
 	}
 
-	@Override
-	@Transactional
-	// 1.若缩略图参数有值，则处理缩略图，
-	// 若原先存在缩略图则先删除再添加新图，之后获取缩略图相对路径并赋值给product
-	// 2.若商品详情图列表参数有值，对商品详情图片列表进行同样的操作
-	// 3.将tb_product_img下面的该商品原先的商品详情图记录全部清除
-	// 4.更新tb_product_img以及tb_product的信息
-	public ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
-			List<ImageHolder> productImgHolderList) throws ProductOperationException {
-		// 空值判断
-		if (product != null && product.getShop() != null && product.getShop().getShopId() != null) {
-			// 给商品设置上默认属性
-			product.setLastEditTime(new Date());
-			// 若商品缩略图不为空且原有缩略图不为空则删除原有缩略图并添加
-			if (thumbnail != null) {
-				// 先获取一遍原有信息，因为原来的信息里有原图片地址
-				Product tempProduct = productDao.queryProductById(product.getProductId());
-				if (tempProduct.getImgAddr() != null) {
-					ImageUtil.deleteFileOrPath(tempProduct.getImgAddr());
-				}
-				addThumbnail(product, thumbnail);
-			}
-			// 如果有新存入的商品详情图，则将原先的删除，并添加新的图片
-			if (productImgHolderList != null && productImgHolderList.size() > 0) {
-				deleteProductImgList(product.getProductId());
-				addProductImgList(product, productImgHolderList);
-			}
-			try {
-				// 更新商品信息
-				int effectedNum = productDao.updateProduct(product);
-				if (effectedNum <= 0) {
-					throw new ProductOperationException("更新商品信息失败");
-				}
-				return new ProductExecution(ProductStateEnum.SUCCESS, product);
-			} catch (Exception e) {
-				throw new ProductOperationException("更新商品信息失败:" + e.toString());
-			}
-		} else {
-			return new ProductExecution(ProductStateEnum.EMPTY);
-		}
-	}
-
+	
 	/**
 	 * 添加缩略图
 	 * 
@@ -171,19 +118,57 @@ public class ProductServiceImpl implements ProductService {
 		}
 	}
 
+	@Override
+	@Transactional
+	//若原来缩略图有值,先删除再添加
+	//详情图同理
+	public ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
+			List<ImageHolder> productImgHolderList) throws ProductOperationException {
+		
+		if(product!=null&&product.getShop()!=null&&product.getShop().getShopId()!=null) {
+			product.setLastEditTime(new Date());
+			//若商品缩略图不为空
+			if(thumbnail!=null) {
+				//先获取一遍原有信息，原有信息里有原图片地址
+				Product tempProduct = productDao.queryProductById(product.getProductId());
+				if(tempProduct.getImgAddr() != null) {
+					ImageUtil.deleteFileOrPath(tempProduct.getImgAddr());
+				}
+				addThumbnail(product,thumbnail);
+			}
+			if(productImgHolderList!=null&&productImgHolderList.size()>0) {
+				deleteProductImgList(product.getProductId());
+				addProductImgList(product,productImgHolderList);
+			}
+			try {
+				
+			
+			int effectedNum = productDao.updateProduct(product);
+			if(effectedNum<=0) {
+				throw new ProductOperationException("更新商品信息失败");
+			}
+			return new ProductExecution(ProductStateEnum.SUCCESS,product);
+			}catch(Exception e) {
+				throw new ProductOperationException("更新商品信息失败"+e.toString());
+			}
+		}else {
+			return new ProductExecution(ProductStateEnum.EMPTY);
+
+		}
+	}
 	/**
-	 * 删除某个商品下的所有详情图
-	 *
+	 * 删除某个商品的所有详情图
 	 * @param productId
 	 */
 	private void deleteProductImgList(long productId) {
-		// 根据productId获取原来的图片
+		//根据productId获取原来的图片
 		List<ProductImg> productImgList = productImgDao.queryProductImgList(productId);
-		// 干掉原来的图片
-		for (ProductImg productImg : productImgList) {
+		//删除原来的图片
+		for(ProductImg productImg:productImgList) {
 			ImageUtil.deleteFileOrPath(productImg.getImgAddr());
 		}
-		// 删除数据库里原有图片的信息
+		//删除数据库里原有图片的信息
 		productImgDao.deleteProductImgByProductId(productId);
 	}
+
 }
